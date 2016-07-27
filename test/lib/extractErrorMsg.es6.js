1130a6ae88971abc1d017e03e6f6d043241304f3
William Ting <wting@reddit.com>
2016-07-27 15:46:44 -0700

    WIP

--- test/lib/extractErrorMsg.es6.js
+++ test/lib/extractErrorMsg.es6.js
@@ -13,22 +13,22 @@ describe('lib: extractErrorMsg', () => {
     expect(extractErrorMsg({errors: [['error type', TEST_MESSAGE]]}))
       .to.equal(TEST_MESSAGE);
   });
-  
+
   it('returns a any message found in error.message', () => {
     expect(extractErrorMsg({message: TEST_MESSAGE}))
     .to.equal(TEST_MESSAGE);
   });
-  
+
   it('returns a generic message for all other cases', () => {
     expect(extractErrorMsg({errors: []}))
       .to.equal(GENERIC_MESSAGE);
-      
+
     expect(extractErrorMsg({errors: [[]]}))
       .to.equal(GENERIC_MESSAGE);
-      
+
     expect(extractErrorMsg(null))
       .to.equal(GENERIC_MESSAGE);
-      
+
     expect(extractErrorMsg({}))
       .to.equal(GENERIC_MESSAGE);
   });
