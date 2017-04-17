f9cc29a8aa10e1b02fc3695a70beeb31b66194e1
Ryan Schwers <schwers.r@gmail.com>
2017-04-17 14:48:49 -0700

    Revert "Add tutorial to home feed for users with 0 subscriptions"
    This reverts commit 43cc5db8f24a4e7974d2c5188763c98055279339.

--- src/app/pages/PostsFromSubreddit.jsx
+++ src/app/pages/PostsFromSubreddit.jsx
@@ -8,7 +8,6 @@ import PostsList from 'app/components/PostsList';
 import NSFWInterstitial from 'app/components/NSFWInterstitial';
 import SortAndTimeSelector from 'app/components/SortAndTimeSelector';
 import SubNav from 'app/components/SubNav';
-import Tutorial from 'app/components/Tutorial';
 import XPromoListingClickModal from 'app/components/XPromoListingClickModal';
 
 import PostsFromSubredditHandler from 'app/router/handlers/PostsFromSubreddit';
@@ -22,16 +21,7 @@ const mapStateToProps = createSelector(
   state => state.subreddits,
   state => state.preferences,
   state => state.modal.id,
-  (state, pageProps) => {
-    const postsListParams = PostsFromSubredditHandler.pageParamsToSubredditPostsParams(pageProps);
-    const isFrontPage = !postsListParams.subredditName;
-    const { loggedOut } = state.user;
-    const { subreddits: subscriptions } = state.subscribedSubreddits;
-    const hasSubs = subscriptions && subscriptions.length;
-
-    return isFrontPage && !loggedOut && !hasSubs;
-  },
-  (pageProps, postsLists, subreddits, preferences, modalId, shouldShowTutorial) => {
+  (pageProps, postsLists, subreddits, preferences, modalId) => {
     const postsListParams = PostsFromSubredditHandler.pageParamsToSubredditPostsParams(pageProps);
     const postsListId = paramsToPostsListsId(postsListParams);
     const { subredditName } = postsListParams;
@@ -43,7 +33,6 @@ const mapStateToProps = createSelector(
       modalId,
       postsList: postsLists[postsListId],
       subreddit: subreddits[subredditName],
-      shouldShowTutorial,
     };
   },
 );
@@ -56,7 +45,6 @@ export const PostsFromSubredditPage = connect(mapStateToProps)(props => {
     subredditName,
     subreddit,
     preferences,
-    shouldShowTutorial,
   } = props;
 
   const showSubnav = !!postsList && !postsList.loading;
@@ -97,14 +85,11 @@ export const PostsFromSubredditPage = connect(mapStateToProps)(props => {
     <div className={ className }>
       { !forFakeSubreddit ? <CommunityHeader subredditName={ subredditName } /> : null }
       { showSubnav ? renderSubNav(subnavLink) : null }
-      { shouldShowTutorial
-        ? <Tutorial />
-        : <PostsList
-            postsListId={ postsListId }
-            subredditIsNSFW={ !!subreddit && subreddit.over18 }
-            subredditShowSpoilers={ !!subreddit && subreddit.spoilersEnabled }
-          />
-      }
+      <PostsList
+        postsListId={ postsListId }
+        subredditIsNSFW={ !!subreddit && subreddit.over18 }
+        subredditShowSpoilers={ !!subreddit && subreddit.spoilersEnabled }
+      />
       <XPromoListingClickModal />
     </div>
   );
